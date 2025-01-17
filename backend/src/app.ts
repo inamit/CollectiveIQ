5fdc6b250369c6a9f9829a3b3bf2155bc6cd66a3
Amit Inbar <inamit323@gmail.com>
2025-01-18 00:21:48 +0200

    revert: revert https

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,25 +1,11 @@
 import initApp from "./server";
-import http from "http";
-import https from "https";
-import fs from "fs";
-
 const port = process.env.PORT || 3000;
 
 initApp()
   .then((app) => {
-    if (process.env.NODE_ENV === "production") {
-      const httpsOptions = {
-        key: fs.readFileSync("../../client-key.pem"),
-        cert: fs.readFileSync("../../client-cert.pem"),
-      };
-      https.createServer(httpsOptions, app).listen(port, () => {
-        console.log(`Server is running on port 443`);
-      });
-    } else {
-      http.createServer(app).listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-      });
-    }
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
   })
   .catch((err) => {
     console.error("Error initializing app", err);
