3ff701158fc4a058536b91ab4a5e0766b508537b
osherlev <osherlev317@gmail.com>
2025-01-18 19:49:30 +0200

    feat: saving and prettifying

--- backend/src/sockets/chat_socket.ts
+++ backend/src/sockets/chat_socket.ts
@@ -1,13 +1,13 @@
 import { Server, Socket } from "socket.io";
-import Chat_model from "../models/chat_model";
+import Chat from "../models/chat_model";
 
 export const chatSocket = (io: Server) => {
     io.on("connection", (socket: Socket) => {
         console.log("User connected:", socket.id);
 
-        socket.on("sendMessage", async ({ senderId, receiverId, message }) => {
+        socket.on("sendMessage", async ({ senderId, receiverId, message,isAi }) => {
             console.log("User sent a message: to:", senderId,receiverId);
-            const chat = new Chat_model({ senderId, receiverId, message });
+            const chat = new Chat({ senderId, receiverId, message,isAi });
             await chat.save();
             console.log("saved sent a message:", message);
             io.to(receiverId).emit("receiveMessage", { senderId, message });
