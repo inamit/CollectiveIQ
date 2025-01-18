1ebfd629e6bb2c3aabb7cfe89468bfb6490cb635
osherlev <osherlev317@gmail.com>
2025-01-18 15:00:41 +0200

    fix:change chat content to message

--- frontend/src/models/chat.ts
+++ frontend/src/models/chat.ts
@@ -2,7 +2,7 @@ import User from "./user.ts";
 
 export default interface Chat {
     _id: string;
-    content: string;
+    message: string;
     receiverId: User;
     senderId: User;
     date: Date;
