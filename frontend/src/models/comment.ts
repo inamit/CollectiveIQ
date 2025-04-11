f3c8b81dab5b8676714d4c529cce2c59ce377d8c
Amit Inbar <inamit323@gmail.com>
2025-04-11 19:03:19 +0300

    feat: add child comments to commentslist

--- frontend/src/models/comment.ts
+++ frontend/src/models/comment.ts
@@ -1,9 +1,11 @@
 import User from "./user.ts";
 
 export default interface Comment {
-    _id: string;
-    content: string;
-    userId: User;
-    date: Date;
-    postID: string;
+  _id: string;
+  content: string;
+  userId: User;
+  date: Date;
+  postID: string;
+  replies?: Comment[];
+  parentCommentID?: string;
 }
