5e5612cdd6acbc92f52ca3fcec1a6ce2686b6920
Osher <osher317@gmail.com>
2025-04-12 14:42:40 +0300

    add front handling to like comments

--- frontend/src/models/comment.ts
+++ frontend/src/models/comment.ts
@@ -1,6 +1,7 @@
 import User from "./user.ts";
+import Likeable from "./likeable.ts";
 
-export default interface Comment {
+export default interface Comment extends Likeable{
     _id: string;
     content: string;
     userId: User;
