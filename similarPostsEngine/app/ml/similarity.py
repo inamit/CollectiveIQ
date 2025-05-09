d38e32b3fd8b7905ff16e0fefd036fb6fd98b405
Amit Inbar <inamit323@gmail.com>
2025-05-09 17:10:14 +0300

    fix: use distinct ids

--- similarPostsEngine/app/ml/similarity.py
+++ similarPostsEngine/app/ml/similarity.py
@@ -3,16 +3,21 @@ from app.ml.model import model
 
 def find_similar_posts(query_text: str, top_k: int = 5):
     query_vec = model.model.encode([query_text], convert_to_numpy=True)
-    D, I = model.index.search(query_vec, top_k)
+    distances, indices = model.index.search(query_vec, top_k)
 
     results = []
-    for score, idx in zip(D[0], I[0]):
-        if idx < len(model.posts):
-            results.append({
-                "id": str(model.posts[idx]["_id"]),
-                "title": model.posts[idx]["title"],
-                "content": model.posts[idx]["content"],
-                "score": float(score)
-            })
+    added_post_ids = set()
+    for score, post_index in zip(distances[0], indices[0]):
+        if post_index < len(model.posts):
+            post_id = str(model.posts[post_index]["_id"])
+            if post_id not in added_post_ids:
+                results.append({
+                    "id": post_id,
+                    "title": model.posts[post_index]["title"],
+                    "content": model.posts[post_index]["content"],
+                    "score": float(score)
+                })
+                added_post_ids.add(post_id)
 
+    results.sort(key=lambda x: x["score"], reverse=True)
     return results
