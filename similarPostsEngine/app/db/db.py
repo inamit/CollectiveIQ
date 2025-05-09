e12c10695cadb68f87826b6a197c0906e7ce908b
Amit Inbar <inamit323@gmail.com>
2025-05-09 16:41:28 +0300

    feat: get posts from db

---
+++ similarPostsEngine/app/db/db.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from pymongo import MongoClient
+
+from app.settings import settings
+
+logger = logging.getLogger('uvicorn.error')
+
+def get_mongo_client():
+    client = MongoClient(settings.DB_URL)
+    return client
+
+
+def get_db():
+    logger.info(f"Connecting to MongoDB...")
+    try:
+        client = get_mongo_client()
+        logger.info("Connected to DB")
+        return client[settings.DB_NAME]
+    except Exception as e:
+        logger.fatal("Couldn't connect to MongoDB: %s", e)
+        return None
