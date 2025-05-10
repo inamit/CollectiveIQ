ccc99165e246c02ddba21327983a8c7b7209d2a8
Osher <osher317@gmail.com>
2025-05-10 09:44:03 +0300

    center things that changed somehow

--- frontend/src/pages/CreatePost/CreatePost.tsx
+++ frontend/src/pages/CreatePost/CreatePost.tsx
@@ -30,67 +30,67 @@ const CreatePost = () => {
     const { request } = postService.saveNewPost(title, question, image);
 
     request
-      .then((response) => {
-        navigate(`${routes.POST}/${response.data._id}`);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+        .then((response) => {
+          navigate(`${routes.POST}/${response.data._id}`);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
   };
   return (
-    <div className="question-container">
-      <h2>Ask a question</h2>
-      <p className="description">
-        Ask a question, or share your knowledge with others
-      </p>
-      <form>
-        <div className="field-container">
-          <label htmlFor="title" className="field-label">
-            Title
-          </label>
-          <AppTextField
-            id="title"
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            fullWidth
-            placeholder="Enter a title"
-          />
-        </div>
+      <div className="question-container">
+        <h2>Ask a question</h2>
+        <p className="description">
+          Ask a question, or share your knowledge with others
+        </p>
+        <form>
+          <div className="field-container">
+            <label htmlFor="title" className="field-label">
+              Title
+            </label>
+            <AppTextField
+                id="title"
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                fullWidth
+                placeholder="Enter a title"
+            />
+          </div>
 
-        <div className="field-container">
-          <label htmlFor="question" className="field-label">
-            What's your question?
-          </label>
+          <div className="field-container">
+            <label htmlFor="question" className="field-label">
+              What's your question?
+            </label>
 
-          <MarkdownEditor
-            content={question}
-            handleInputChange={(_, content) => setQuestion(content)}
-          />
-        </div>
+            <MarkdownEditor
+                content={question}
+                handleInputChange={(_, content) => setQuestion(content)}
+            />
+          </div>
 
-        <ImagePicker image={image} setImage={setImage} required={false} />
-        <div className="button-container">
-          <Button
-            type="submit"
-            onClick={handleAddPost}
-            className="submit-button"
-            color="primary"
-            variant="contained"
-          >
-            Ask
-          </Button>
-          <Button
-            type="button"
-            onClick={handleCancel}
-            color="secondary"
-            variant="contained"
-          >
-            Cancel
-          </Button>
-        </div>
-      </form>
-    </div>
+          <ImagePicker image={image} setImage={setImage} required={false} />
+          <div className="button-container">
+            <Button
+                type="submit"
+                onClick={handleAddPost}
+                className="submit-button"
+                color="primary"
+                variant="contained"
+            >
+              Ask
+            </Button>
+            <Button
+                type="button"
+                onClick={handleCancel}
+                color="secondary"
+                variant="contained"
+            >
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </div>
   );
 };
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: "./ssl/client-key.pem",
-      cert: "./ssl/client-cert.pem",
-    },
-    port: 443,
+    https: false,
   },
+
 });
