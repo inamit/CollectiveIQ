e56467ccb3655ed15e14e8f2a1d8770266cdbf94
Amit Inbar <inamit323@gmail.com>
2025-05-17 15:02:04 +0300

    feat: use input classes

--- similarPostsEngine/app/main.py
+++ similarPostsEngine/app/main.py
@@ -1,6 +1,9 @@
 import logging
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Body
 from contextlib import asynccontextmanager
+
+from app.input.addPostInput import AddPostInput
+from app.input.similarPostsInput import SimilarPostsInput
 from app.ml.model import model
 from app.db.postsService import fetch_posts_from_db, fetch_post_by_id
 from app.ml.similarity import find_similar_posts
@@ -19,13 +22,13 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/similar-posts")
-async def similar_posts(title: str = Query(...), content: str = Query(...), top_k: int = 5):
-    query_text = f"{title} {content}"
-    return find_similar_posts(query_text, top_k)
+async def similar_posts(body: SimilarPostsInput = Body(...)):
+    query_text = f"{body.title} {body.content}"
+    return find_similar_posts(query_text, body.top_k)
 
 
 @app.post("/add-post")
-async def add_post(post_id: str = Query(...)):
-    new_post = fetch_post_by_id(post_id)
+async def add_post(body: AddPostInput = Body(...)):
+    new_post = fetch_post_by_id(body.post_id)
     model.add_posts_to_index([new_post])
     return {"message": "Post added successfully"}
