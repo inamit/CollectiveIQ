9b3604e7396e040e5eb628ad2a2ef22043c0e5a9
Osher <osher317@gmail.com>
2025-06-01 14:25:27 +0300

    revert vite config for ssl

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    https: {
+      key: "./ssl/client-key.pem",
+      cert: "./ssl/client-cert.pem",
+    },
+    port: 443,
+  },
 });
