c53f1c83fea1cc1d8478ed4324a968db0de01879
Amit Inbar <inamit323@gmail.com>
2025-06-10 17:43:20 +0300

    fix: refreshposts on load

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,20 @@
-import PostsList from '../../components/PostsList/PostsList';
-import './HomePage.css';
+import PostsList from "../../components/PostsList/PostsList";
+import "./HomePage.css";
 import { usePostsContext } from "../../context/postsContext";
+import { useEffect } from "react";
 
 const HomePage = () => {
-  const { filteredPosts } = usePostsContext();
-  
+  const { filteredPosts, reloadPosts } = usePostsContext();
+
+  useEffect(() => {
+    reloadPosts();
+  }, []);
+
   return (
-      <div className="homepage-container">
-        <h1 className="homepage-title">CollectiveIQ</h1>
-        <PostsList posts={filteredPosts || []} maxPostsPerPage={3} />
-      </div>
+    <div className="homepage-container">
+      <h1 className="homepage-title">CollectiveIQ</h1>
+      <PostsList posts={filteredPosts || []} maxPostsPerPage={3} />
+    </div>
   );
 };
 
